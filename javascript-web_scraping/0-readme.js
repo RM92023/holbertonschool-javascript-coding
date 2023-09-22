902c12988d291c76e2383e6932162362b37341f3
RM92023 <robinjara20@gmail.com>
2023-09-22 10:49:52 -0500

    update styles

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
-const fs = require("fs");
+const fs = require('fs');
 const fileName = argv[2];
-fs.readFile(fileName, "utf8", function (err, data) {
+fs.readFile(fileName, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
